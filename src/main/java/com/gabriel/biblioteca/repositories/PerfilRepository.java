fbeb680f84012ae90fe89b434ae07f389b1e1fb3
Gabriel dos Santos <gabriel.msamax@gmail.com>
2024-09-10 14:32:17 -0300

    Criando os Repositories

---
+++ src/main/java/com/gabriel/biblioteca/repositories/PerfilRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.biblioteca.repositories;
+
+import com.gabriel.biblioteca.models.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+}
