1a054fd7944af4f5e018589aae08a4cc16f9b49b
Gabriel dos Santos <gabriel.msamax@gmail.com>
2024-09-12 15:50:09 -0300

    Criando os Controllers

---
+++ src/main/java/com/gabriel/biblioteca/controllers/CategoriaController.java
@@ -0,0 +1,41 @@
+package com.gabriel.biblioteca.controllers;
+
+import com.gabriel.biblioteca.models.Categoria;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getById(@PathVariable long id){
+        return ResponseEntity.ok().body(null);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> getAll(){
+        return ResponseEntity.ok().body(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria categoria){
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand('1').toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Categoria> update(@PathVariable long id, @RequestBody Categoria categoria){
+        return ResponseEntity.ok().body(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable long id){
+        return ResponseEntity.noContent().build();
+    }
+}
